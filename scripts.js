13e9fa5438eb224bc8cad87800d495ce345c99c1
kyle-86 <kyle@thirteendigital.com.au>
2022-02-24 15:27:45 +1000

    Reworked redirect in JS

--- scripts.js
+++ scripts.js
@@ -8,21 +8,7 @@ jQuery(document).ready(function($) {
 
     $.magnificPopup.close()
 
-    // Magnific Popup - Standard
-
-    $('.js-magnific-location').magnificPopup({
-        type: 'inline',
-        closeBtnInside: true,
-        midClick: true,
-        callbacks: {
-            close: function() {
-                if (!Cookies.get('cookie-location')) { // If no cookie is set
-                    var bodyLocation = $('body').attr('data-location'); // Set location
-                    Cookies.set('cookie-location', bodyLocation, { expires: 30 });
-                }
-            }
-        }
-    });      
+    // Magnific Popup - Standard     
 
     var user_agent = navigator.userAgent;
     // var is_bot = /bot|google|baidu|bing|msn|duckduckbot|teoma|slurp|yandex/i.test(navigator.userAgent);
@@ -30,7 +16,7 @@ jQuery(document).ready(function($) {
     var is_bot = navigator.userAgent.indexOf("Googlebot") != -1;
 
     if (navigator.userAgent.indexOf("Googlebot") != -1) {        
-        console.log(user_agent);
+        //console.log(user_agent);
     } else {        
         console.log(user_agent);
         if (!Cookies.get('cookie-location')) {
@@ -53,6 +39,7 @@ jQuery(document).ready(function($) {
 
     $('.js-cookie-location').click(function() {
         var location = $(this).attr('data-location');
+        console.log(location);
         if (!Cookies.get('cookie-location')) { // If the user hasn't set a location yet
             Cookies.set('cookie-location', location, { expires: 30 });
         } else if (Cookies.get('cookie-location')) {
@@ -60,10 +47,48 @@ jQuery(document).ready(function($) {
             Cookies.set('cookie-location', location, { expires: 30 });
         }
     });
-
      if (Cookies.get('cookie-location')) { // Else if they have, redirect
-         var cookieLocation = Cookies.get('cookie-location');
-         var currentURL = window.location.href;
+        var cookieLocation = Cookies.get('cookie-location');
+        cookieLocation = cookieLocation.toLowerCase();
+        var currentURL = window.location.href;
+
+        var windowPath = window.location.pathname;
+        var start_pos = windowPath.indexOf('/') + 1;
+        var end_pos = windowPath.indexOf('/',start_pos);
+        var storeID = windowPath.substring(start_pos,end_pos);
+
+        //console.log(storeID);
+
+        if (storeID != 'au' && storeID != 'eu' && storeID != 'uk' ) {
+            storeID = 'us';
+        }
+
+        //console.log(storeID);
+
+        if ( cookieLocation == storeID ) {
+            console.log('cookie matches store');
+             var baseURL = window.location.origin;
+            var sameLink = window.location.pathname;
+            var newURL = sameLink.replace(storeID, cookieLocation);
+            //console.log(baseURL + newURL);
+        } else { 
+            var baseURL = window.location.origin;
+            var sameLink = window.location.pathname;
+
+            var newURL = '';
+
+            if (cookieLocation == 'us') {
+                newURL = sameLink.replace(storeID, '');
+                newURL = baseURL + newURL;
+            } else if (  storeID = 'us' ) {
+                newURL = baseURL + '/' + cookieLocation + '/' +  sameLink;
+            } else {
+                newURL = sameLink.replace(storeID, cookieLocation);
+                newURL = baseURL + newURL;
+            }
+
+            window.location.href = newURL;
+        }
      }
 
     });
